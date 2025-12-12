33fa62d69a4473417ad06797fba8b5428b17270f
Thanveer Ahmed <thanveer.a@enfintechnologies.com>
2025-12-12 17:05:44 +0530

    Tokenised request

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,26 +8,33 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-// Email Template Routes
-$routes->group('api/email-templates', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('current_time', 'EmailTemplateController::current_time');
-    $routes->get('/', 'EmailTemplateController::index');
-    $routes->get('active', 'EmailTemplateController::getActive');
-    $routes->get('by-code/(:segment)', 'EmailTemplateController::getByCode/$1');
-    $routes->get('(:num)', 'EmailTemplateController::show/$1');
-    $routes->post('/', 'EmailTemplateController::create');
-    $routes->put('(:num)', 'EmailTemplateController::update/$1');
-    $routes->delete('(:num)', 'EmailTemplateController::delete/$1');
+ $routes->group('api/debug', ['namespace' => 'App\Controllers'], function($routes) {
+    $routes->get('/', 'DebugController::index');
+    $routes->get('current_time', 'DebugController::current_time');
+    $routes->post('headers', 'DebugController::headers');
 });
 
-// Email Queue Routes
-$routes->group('api/email-queue', ['namespace' => 'App\Controllers'], function($routes) {
-    $routes->get('/', 'EmailQueueController::index');
-    $routes->get('statistics', 'EmailQueueController::statistics');
-    $routes->get('(:num)', 'EmailQueueController::show/$1');
-    $routes->post('/', 'EmailQueueController::create');
-    $routes->put('(:num)', 'EmailQueueController::update/$1');
-    $routes->delete('(:num)', 'EmailQueueController::delete/$1');
-    $routes->post('(:num)/retry', 'EmailQueueController::retry/$1');
-    $routes->post('run_scheduler', 'EmailQueueController::run_scheduler');
+$routes->group('api', ['filter' => 'apiSignature', 'namespace' => 'App\Controllers'], function($routes) {
+    // Email Template Routes
+    $routes->group('email-templates', ['namespace' => 'App\Controllers'], function($routes) {
+        $routes->get('/', 'EmailTemplateController::index');
+        $routes->get('active', 'EmailTemplateController::getActive');
+        $routes->get('by-code/(:segment)', 'EmailTemplateController::getByCode/$1');
+        $routes->get('(:num)', 'EmailTemplateController::show/$1');
+        $routes->post('/', 'EmailTemplateController::create');
+        $routes->put('(:num)', 'EmailTemplateController::update/$1');
+        $routes->delete('(:num)', 'EmailTemplateController::delete/$1');
+    });
+
+    // Email Queue Routes
+    $routes->group('email-queue', ['namespace' => 'App\Controllers'], function($routes) {
+        $routes->get('/', 'EmailQueueController::index');
+        $routes->get('statistics', 'EmailQueueController::statistics');
+        $routes->get('(:num)', 'EmailQueueController::show/$1');
+        $routes->post('/', 'EmailQueueController::create');
+        $routes->put('(:num)', 'EmailQueueController::update/$1');
+        $routes->delete('(:num)', 'EmailQueueController::delete/$1');
+        $routes->post('(:num)/retry', 'EmailQueueController::retry/$1');
+        $routes->post('run_scheduler', 'EmailQueueController::run_scheduler');
+    });
 });
